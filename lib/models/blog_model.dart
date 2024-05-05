62d254bfa3d55e4f67275d6ad86fe91516718b6c
xudayfi <xudayfiibraahim8@gmail.com>
2024-05-06 00:12:01 +0300

    added some files

---
+++ lib/models/blog_model.dart
@@ -0,0 +1,15 @@
+class BlogLayout {
+  final String title;
+  final String author;
+  final String description;
+  final String body;
+  final bool is_published;
+
+  BlogLayout({
+    required this.title,
+    required this.author,
+    required this.description,
+    required this.body,
+    required this.is_published,
+  });
+}
